e67b865448d4be379bf0e01d3025eeeda6add575
Alex Lim <kaitwee@gmail.com>
2025-06-17 19:44:57 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../css/App.css'
+import '../css/button-3.css'
+//import {Buttons} from './buttons'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className="button-3" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        The current time is {currentTime}.
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/buttons.jsx
@@ -0,0 +1,16 @@
+import '../css/button-3.css';
+
+export default function Buttons()
+{
+  return (
+    <div className="container">
+      <h1 className="title">Modern Button Panel</h1>
+      <div className="buttonGrid">
+        <button className="button-3">Button 1</button>
+        <button className="button-3">Button 2</button>
+        <button className="button-3">Button 3</button>
+        <button className="button-3">Button 4</button>
+      </div>
+    </div>
+  );
+};
