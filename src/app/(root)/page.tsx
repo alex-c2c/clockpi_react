c976239ea9b7e495b249cd69da0855e4cbacd8f0
Alex Lim <kaitwee@gmail.com>
2025-08-31 22:33:51 +0800

    remove timediv from dashboard

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import TimeDiv from "@/app/(root)/components/TimeDiv";
 import EpdDiv from "@/app/(root)/components/EpdDiv";
 import CarouselDiv from "@/app/(root)/components/CarouselDiv";
 import ScheduleDiv from "@/app/(root)/components/ScheduleDiv";
@@ -6,7 +5,6 @@ import ScheduleDiv from "@/app/(root)/components/ScheduleDiv";
 export default function App() {
 	return (
 		<div className="flex gap-4 flex-col items-center bg-stone-1000 my-4">
-			<TimeDiv />
 			<EpdDiv />
 			<CarouselDiv />
 			<ScheduleDiv />
